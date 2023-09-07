ff47cce9d8aa990d276a30c16ea0ec0d44f4a69b
Yun-ss <106048793+Yun-ss@users.noreply.github.com>
2023-09-07 10:37:18 +0900

    pyramid

--- Cprac1-1/main.cpp
+++ Cprac1-1/main.cpp
@@ -4,7 +4,7 @@ int main() {
 	int n;
 	int i, j, k;
 	scanf_s("%d", &n);
-	printf("%d층 피라미드 \n",n);
+	printf("%d층 피라미드\n",n);
 	for (i = 1; i <= n; i++) {
 		for (j = 1; j <= n-i; j++) {
 			printf(" ");
