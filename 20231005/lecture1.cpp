0acb8609b53a4caf1672b1d4e415d13cbb95bb75
Yun-ss <106048793+Yun-ss@users.noreply.github.com>
2023-10-05 09:54:31 +0900

    Monte Carlo Algorithm
    add comment

--- 20231005/lecture1.cpp
+++ 20231005/lecture1.cpp
@@ -15,6 +15,9 @@ int main(void) {
 		y = (double)rand() / (double)RAND_MAX;
 		count++;
 		if ((pow(x, 2) + pow(y, 2)) <= 1) {
+			/*
+      			x^2+y^2 <= r^2 -> x^2+y^2 <= 1 일때 (x,y)는 원 위에 있음
+      			*/
 			inCicle++;
 		}
 		phi = 4*(float)inCicle / (float)count;	
